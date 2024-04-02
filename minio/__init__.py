347605873842ee6744a56969f07a78b85904b118
Alan P John <alansandra2013@gmail.com>
2024-04-02 18:46:11 +0100

    patch: Update __init__.py
    To avoid importing argon2

--- minio/__init__.py
+++ minio/__init__.py
@@ -42,4 +42,4 @@ from .api import Minio as Minio
 from .error import InvalidResponseError as InvalidResponseError
 from .error import S3Error as S3Error
 from .error import ServerError as ServerError
-from .minioadmin import MinioAdmin as MinioAdmin
+# from .minioadmin import MinioAdmin as MinioAdmin
